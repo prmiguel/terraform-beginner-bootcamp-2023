55bc73fa1bb32b9a3a5068d0d055374636cd42a1
prmiguel <miangel-pr@outlook.com>
2023-10-09 08:23:02 +0000

    Provisioner to invalidate cache

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "bucket_name" {
 output "s3_static_endpoint" {
   value = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
+}
