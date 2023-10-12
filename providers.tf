9c3620791d7c9feae20b716193a80f28d8a916de
prmiguel <miangel-pr@outlook.com>
2023-10-12 22:21:31 +0000

    Add provider block using our terraform custom provider

--- providers.tf
+++ providers.tf
@@ -1,4 +1,10 @@
 terraform {
+   required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
 #   cloud {
 #     organization = "MPTerraformBootcamp"
 
